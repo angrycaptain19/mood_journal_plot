af4b76a9771cb8e00817d350ad68a862aff8bb3f
Sourcery AI <>
2021-03-02 09:52:09 +0000

    'Refactored by Sourcery'

--- graphing_service.py
+++ graphing_service.py
@@ -9,16 +9,18 @@ style.use('ggplot')
 class GraphingService:
     def get_graph_dataframe(self, data_dict):
         pd.set_option("display.max_rows", None, "display.max_columns", None)
-        sorted_dataframe = pd.DataFrame.from_dict(data_dict, orient='index', columns=['sentiment']).sort_index()
-        return sorted_dataframe
+        return pd.DataFrame.from_dict(
+            data_dict, orient='index', columns=['sentiment']
+        ).sort_index()
 
     def plot_graph(self, data_dict, username):
         graph_df = self.get_graph_dataframe(data_dict)
         print(graph_df)
         y_values = graph_df['sentiment'].values
-        x_values = []
-        for index in graph_df.index:
-            x_values.append(TimeService().get_string_from_timestamp(index))
+        x_values = [
+            TimeService().get_string_from_timestamp(index)
+            for index in graph_df.index
+        ]
 
         print(x_values)
         print(y_values)
